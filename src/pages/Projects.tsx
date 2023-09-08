a32d881fc7c221a39a34c64fed127361e2e5b1ea
vit-byro <vitbyro@gmail.com>
2023-09-08 16:13:56 +0200

    fixed line breaks

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -7,13 +7,13 @@ export default function Projects() {
       text: `Portfolio
 
       This website runs on Nginx on my Ubuntu server in my home. No other way for full control in my opinion. I created it in React with Typescript, formatting is mainly executed with Tailwind, but one can't avoid CSS. Check out the Git repository if you are interested, I made it public!`,
-      img: "secretary.jpg",
+      img: "projects.jpg",
     },
     {
       text: `Currency
       
 This one is a collaboration project. An API is under creation for exchange rates and historical data sets of currencies based on the daily rates stated by Sweden's central bank. Lot of such APIs are available, but none of them are reliable and mainly free enough for our future plans.`,
-      img: "secretary.jpg",
+      img: "money.jpg",
     },
   ];
   let align: boolean = true;
