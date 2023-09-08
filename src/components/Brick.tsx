439bde999beffe2d37882e62090e3b5e48cd0651
vit-byro <guti.csaba.ferenc@gmail.com>
2023-09-08 16:03:06 +0200

    fixed line breaks

--- src/components/Brick.tsx
+++ src/components/Brick.tsx
@@ -8,9 +8,9 @@ export default function Brick({ block, align, index, length }: Props) {
       className="flex flex-col items-center justify-center"
     >
       <div className={`flex h-[70vh] ${align ? " flex-row-reverse" : ""} `}>
-        <p className="p-10 w-1/2 text-[1.5vw] text-left first-letter:[font-size:3em] first-letter:float-left first-letter:leading-[88%] first-letter:mr-[0.1em] first-letter:[text-shadow:_-0.1em_-0.1em_0.1em_rgb(128_128_128_/_1)]">
+        <span className="whitespace-pre-line p-10 w-1/2 text-[1.5vw] text-left first-letter:[font-size:3em] first-letter:float-left first-letter:leading-[88%] first-letter:mr-[0.1em] first-letter:[text-shadow:_-0.1em_-0.1em_0.1em_rgb(128_128_128_/_1)]">
           {block.text}
-        </p>
+        </span>
         <div className="w-1/2 m-10 flex justify-center items-center overflow-clip">
           <img
             src={require(`../img/${block.img}`)}
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,3 +1,36 @@
+import { Block } from "../types";
+import Brick from "../components/Brick";
+
 export default function Projects() {
-  return <div>Projects</div>;
+  const blocks: Block[] = [
+    {
+      text: `Portfolio
+
+      This website runs on Nginx on my Ubuntu server in my home. No other way for full control in my opinion. I created it in React with Typescript, formatting is mainly executed with Tailwind, but one can't avoid CSS. Check out the Git repository if you are interested, I made it public!`,
+      img: "secretary.jpg",
+    },
+    {
+      text: `Currency
+      
+This one is a collaboration project. An API is under creation for exchange rates and historical data sets of currencies based on the daily rates stated by Sweden's central bank. Lot of such APIs are available, but none of them are reliable and mainly free enough for our future plans.`,
+      img: "secretary.jpg",
+    },
+  ];
+  let align: boolean = true;
+
+  return (
+    <div className="text-white">
+      {blocks.map(function (block: Block, index) {
+        align = !align;
+        return (
+          <Brick
+            block={block}
+            align={align}
+            index={index}
+            length={blocks.length}
+          />
+        );
+      })}
+    </div>
+  );
 }
